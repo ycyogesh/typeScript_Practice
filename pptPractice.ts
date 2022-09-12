210f29db73ef7b2abc4b6343958c671b93e80aff
ycyogesh <ycyogesh183@gmail.com>
2022-09-12 23:06:35 +0530

    Arrays

--- pptPractice.ts
+++ pptPractice.ts
@@ -20,12 +20,27 @@ console.log(employeeName);
 // Array Datatype 
 
 let fruits : string[] = ["Apple","Orange","Banana"];        // normal
-let fruits1 : Array<string> = ["Apple","Orange","Banana"];  // generic array type
+let fruits1 : Array<string> = ["Grapes","Pinapple","Guava"];  // generic array type
 let arr = [1,3,"Apple","Orange","Banana",true,false]        // this also can
 
 console.log(fruits);
 console.log(fruits1);
 console.log(arr);
+
+let fruits2 : Array<string>;
+fruits2 = ["Mango","Jackfruit","Lemon"];    // string array
+console.log(fruits2);
+
+let ids : Array<number>;
+ids = [23,34,100,124,44];                   // number array
+console.log(ids);
+
+let values : (string | number)[] = [1,"Cucumber",2,"Custard Apple"]
+let values1 : Array<string | number> = [3,"Papaya",4,"Sapota"]
+console.log(values);
+console.log(values1);
+
+
 
 
 
