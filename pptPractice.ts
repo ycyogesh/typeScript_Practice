a81840feb52620854b1482ba8c73e3d08367d9f0
ycyogesh <ycyogesh183@gmail.com>
2022-09-12 22:48:50 +0530

    Number types

---
+++ pptPractice.ts
@@ -0,0 +1,15 @@
+// Number type variables
+
+let first : number = 123;       // number
+let second : number = 0x37CF;    // hexadecimal
+let third : number = 0o377;     // octal
+let fourth : number = 0b111001; // binary
+
+console.log(first);
+console.log(second);
+console.log(third);
+console.log(fourth);
+
+
+// 
+
