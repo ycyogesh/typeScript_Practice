976a3942003f2f566d40d208b990fbe01c7bca99
ycyogesh <ycyogesh183@gmail.com>
2022-09-13 16:31:23 +0530

    Implementing an Interface

--- pptPractice.ts
+++ pptPractice.ts
@@ -267,4 +267,22 @@ console.log(personObj);
 
 // Implmenting an Interface
 
+interface IEmployee1 {
+    empCode : number;
+    name : string;
+    getSalary : (arg0: number)=>number;
+}
 
+class Employee implements IEmployee1{
+    empCode : number;
+    name : string;
+
+    constructor (code : number, name : string){
+        this.empCode = code;
+        this.name = name;
+    }
+    getSalary(empCode: number) : number{
+        return 2000;
+    }
+}
+let emp = new Employee(1,"Shamili");
