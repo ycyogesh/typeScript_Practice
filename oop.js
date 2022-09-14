6bbb0f876026c44a4fa87a6fb436ecb9dc296071
ycyogesh <ycyogesh183@gmail.com>
2022-09-14 19:20:35 +0530

    added codes

--- oop.js
+++ oop.js
@@ -1,10 +1,30 @@
+var a = 10;
 var Calculator = /** @class */ (function () {
+    // constructor (a : number, b : number){
+    //     this.a = a;
+    //     this.b = b;
+    // }
     function Calculator(a, b) {
+        if (b != null && typeof (b) == "number") {
+            this.b = b;
+        }
         this.a = a;
-        this.b = b;
     }
+    // constructor(a : number | string){
+    //     this.a = a;
+    // }
     Calculator.prototype.sum = function () {
-        return this.a + this.b;
+        // return this.a;  // return a
+        if (typeof (this.a) == "number") {
+            return this.a + this.b;
+        }
+        return 0;
+    };
+    Calculator.prototype.multiply = function () {
+        if (typeof (this.a) == "number") {
+            return 5 * this.sum();
+        }
+        return 0;
     };
     return Calculator;
 }());
@@ -12,3 +32,8 @@ var c = new Calculator(2, 4);
 console.log(c.a);
 console.log(c.b);
 console.log(c.sum());
+console.log(c.multiply());
+// function abc(a : number){
+// }
+// function abc(b : number){
+// }
--- oop.ts
+++ oop.ts
@@ -1,19 +1,37 @@
 let a : number = 10
 class Calculator{
-    a : number;
-    b : number;
+    a : number | string;
+    b! : number;
+    constructor(a : number);
+    constructor(a : string);
+    constructor(a : number, b : number);
 
-    constructor (a : number, b : number){
+    // constructor (a : number, b : number){
+    //     this.a = a;
+    //     this.b = b;
+    // }
+    constructor (a : number | string, b? : number){
+        if(b != null && typeof(b)=="number"){
+            this.b = b;
+        } 
         this.a = a;
-        this.b = b;
     }
+    // constructor(a : number | string){
+    //     this.a = a;
+    // }
     sum(){
-        // return a;
-        return this.a + this.b;
+        // return this.a;  // return a
+        if(typeof(this.a)=="number"){
+            return this.a + this.b;
+        }
+        return 0;
     }
 
     multiply(){
-        return 5 * this.sum();
+        if(typeof(this.a)=="number"){
+            return 5 * this.sum();
+        }
+        return 0;
     }
 }
 let c : Calculator = new Calculator(2,4);
@@ -22,5 +40,10 @@ console.log(c.b);
 console.log(c.sum());
 console.log(c.multiply());
 
+// function abc(a : number){
 
+// }
+// function abc(b : number){
+
+// }
 
