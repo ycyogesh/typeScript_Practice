2d34da8d0c32acc75b55092efb53fd0bc8cc6044
ycyogesh <ycyogesh183@gmail.com>
2022-09-14 12:36:34 +0530

    checking assigning

--- my1.js
+++ my1.js
@@ -6,3 +6,19 @@ else{
     console.log("False");
     
 }
+
+a = [1,2,3,7]
+// b = a                    // changed old(a) array
+// b = Object.assign(a)     // changed old(a) array
+b = [...a]               // not changed the old(a) array     correct method
+
+b[2] = 8
+console.log(a);
+console.log(b);
+practice(a);
+function practice(passedArrray){
+    passedArrray[0] = 20
+    // return passedArrray
+}
+
+console.log(a);
