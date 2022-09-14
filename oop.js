aa02b86bdbdb819755c56c080a924bd7962cca8f
ycyogesh <ycyogesh183@gmail.com>
2022-09-14 17:45:46 +0530

    compiled

---
+++ oop.js
@@ -0,0 +1,10 @@
+var Calculator = /** @class */ (function () {
+    function Calculator(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+    return Calculator;
+}());
+var c = new Calculator(2, 4);
+console.log(c.a);
+console.log(c.b);
--- oop.ts
+++ oop.ts
@@ -1,3 +1,13 @@
 class Calculator{
-    
+    a : number;
+    b : number;
+
+    constructor (a : number, b : number){
+        this.a = a;
+        this.b = b;
+    }
 }
+let c : Calculator = new Calculator(2,4);
+console.log(c.a);
+console.log(c.b);
+
